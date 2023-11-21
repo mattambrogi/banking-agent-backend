7697d75df2995bfcf140f8640b84c3213162ffb0
Matt <matthew.ambrogi@gmail.com>
2023-11-21 10:34:05 -0500

    remove redundant file reads

--- llama_agent.py
+++ llama_agent.py
@@ -4,8 +4,6 @@ load_dotenv()
 import pandas as pd
 
 def get_llama_agent ():
-    df = pd.read_json('mock-transactions.json')
-    df.to_csv('transactions.csv', index=False)
     agent = OpenAIAssistantAgent.from_new(
         name="Banking Analysis Assistant",
         instructions="You are a personal assistant which analyzes a users banking data. You take the user query and analyze the provided bank transactions file to get an answer. Keep responses brief and concise. Answers should be no more than 70 words.",
