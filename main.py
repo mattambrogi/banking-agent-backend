bc3382486730d3be5e3ed4c2cd7975bed7653af6
Matt <matthew.ambrogi@gmail.com>
2023-11-21 12:18:34 -0500

    add retry logic to endpoint

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from llama_agent import get_llama_agent
+import asyncio
 
 app = FastAPI()
 
@@ -26,23 +27,32 @@ def read_root():
     print("test")
     return {"message": "Hello, World!"}
 
+max_retries = 3
+backoff_factor = 2
 
 agent = None
+
 @app.post("/chat")
 async def chat_endpoint(request_data: dict):
     global agent
-    try:
-        user_input = request_data.get("user_input", "")
-        if agent is None:
-            # Create the agent if it hasn't been created yet
-            agent = get_llama_agent()
-        response = agent.chat(user_input)
-        return {"aiResponse": str(response)}
-    except Exception as e:
-        # Log the exception
-        print(f"Error in chat_endpoint: {e}")
-        # Return a 500 error response
-        return {"error": "Internal Server Error"}, 500
+    retries = 0
+    while retries < max_retries:
+        try:
+            user_input = request_data.get("user_input", "")
+            if agent is None:
+                # Create the agent if it hasn't been created yet
+                agent = get_llama_agent()
+            response = agent.chat(user_input)
+            return {"aiResponse": str(response)}
+        except Exception as e:
+            retries += 1
+            # Log the exception
+            print(f"Error in chat_endpoint: {e}")
+            if retries < max_retries:
+                await asyncio.sleep(backoff_factor ** retries)
+            else:
+            # Return a 500 error response
+                return {"error": "Internal Server Error"}, 500
 
 
 
